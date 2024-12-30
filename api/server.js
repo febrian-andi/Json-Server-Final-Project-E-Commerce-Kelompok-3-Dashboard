f6eb67bd9a7626232ca1eae7b2436426a1eb8b71
Febrian Andi Nugroho <febrianandinugroho@gmail.com>
2024-12-30 22:23:32 +0700

    commit

--- api/server.js
+++ api/server.js
@@ -1,42 +1,31 @@
+// See https://github.com/typicode/json-server#module
 const jsonServer = require('json-server')
-const fs = require('fs')
-const path = require('path')
 
 const server = jsonServer.create()
 
-// Membaca data dari db.json
-const filePath = path.join(__dirname, '..', 'db.json')
-const data = fs.readFileSync(filePath, "utf-8")
-const db = JSON.parse(data)
+// Uncomment to allow write operations
+// const fs = require('fs')
+// const path = require('path')
+// const filePath = path.join('db.json')
+// const data = fs.readFileSync(filePath, "utf-8");
+// const db = JSON.parse(data);
+// const router = jsonServer.router(db)
 
-// Menggunakan router dengan db yang sudah dibaca
-const router = jsonServer.router(db)
+// Comment out to allow write operations
+const router = jsonServer.router('db.json')
 
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-
-// Menambahkan middleware untuk menulis perubahan ke db.json setelah setiap operasi
-server.use((req, res, next) => {
-  const oldSend = res.send
-  res.send = (body) => {
-    // Jika body berisi data (misalnya, setelah POST/PUT/DELETE)
-    if (body && body !== '[]') {
-      // Mengupdate db.json dengan data terbaru
-      fs.writeFileSync(filePath, JSON.stringify(db, null, 2))
-    }
-    // Melanjutkan alur pengiriman respons
-    oldSend.call(res, body)
-  }
-  next()
-})
-
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
-
-// Menjalankan server pada port 3000
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
 
-// Mengekspor API server jika diperlukan
+// Export the Server API
 module.exports = server
